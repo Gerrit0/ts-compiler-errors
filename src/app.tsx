7a161980bb24ebeab332d56a14e22502966b00b2
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2019-05-27 15:04:10 -0600

    Searching works

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,39 @@
-import { h } from 'preact'
-import { ErrorView } from './error-view'
+import { h, Component } from 'preact'
 import errors from '../data/errors.json'
+import lunr from 'lunr'
+import { ErrorView } from './error-view'
+import { debounce } from './utils'
 
-export function App () {
-  return <div>
-    {errors.map(err => <ErrorView error={err} />)}
-  </div>
+export class App extends Component<{}, { search: string, results: lunr.Index.Result[] }> {
+  readonly index: lunr.Index
+
+  constructor () {
+    super()
+
+    this.index = lunr(function () {
+      this.field('code', { boost: 5 })
+      this.field('message')
+
+      errors.forEach((err, i) => this.add({ id: i, ...err }))
+    })
+
+    this.state = {
+      search: '',
+      results: this.index.search('')
+    }
+  }
+
+  render () {
+    return <div>
+      <input value={this.state.search} onInput={debounce(this.updateSearch, 250)}/>
+      {this.state.results.map(r => <ErrorView error={errors[+r.ref]} />)}
+    </div>
+  }
+
+  updateSearch = (e: Event) => {
+    const search: string = (e.target as any).value
+    const results = this.index.search(search)
+    console.log(search, results)
+    this.setState({ search, results })
+  }
 }
